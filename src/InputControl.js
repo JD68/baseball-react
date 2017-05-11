edcf904766e51f6b502c3b282b1001f3ae4c6765
Jeff Dufresne <jeffery.dufresne@gmail.com>
2017-05-11 18:26:04 -0400

    layout of input control

--- src/InputControl.js
+++ src/InputControl.js
@@ -5,25 +5,35 @@ class InputControl extends Component {
   render() {
     return (
       <div>
-        <DropdownButton title="Year" id="years-nested-dropdown">
-          <MenuItem eventKey="1">2014</MenuItem>
-          <MenuItem eventKey="2">2013</MenuItem>
-        </DropdownButton>
-        <DropdownButton title="League" id="leagues-nested-dropdown">
-          <MenuItem eventKey="1">American</MenuItem>
-          <MenuItem eventKey="2">National</MenuItem>
-        </DropdownButton>
-        <DropdownButton title="Division" id="leagues-nested-dropdown">
-          <MenuItem eventKey="1">East</MenuItem>
-          <MenuItem eventKey="2">Central</MenuItem>
-          <MenuItem eventKey="2">West</MenuItem>
-        </DropdownButton>
-        <DropdownButton title="View" id="view-nested-dropdown">
-          <MenuItem eventKey="1">Batting Average</MenuItem>
-          <MenuItem eventKey="2">ERA</MenuItem>
-          <MenuItem eventKey="2"></MenuItem>
-        </DropdownButton>
-        <Button href="/foo/bar" className="btn btn-default">Foo</Button>
+        <div className="col-md-1">
+          <DropdownButton title="Year" id="years-nested-dropdown">
+            <MenuItem eventKey="1">2014</MenuItem>
+            <MenuItem eventKey="2">2013</MenuItem>
+          </DropdownButton>
+        </div>
+        <div className="col-md-1">
+          <DropdownButton title="League" id="leagues-nested-dropdown">
+            <MenuItem eventKey="1">American</MenuItem>
+            <MenuItem eventKey="2">National</MenuItem>
+          </DropdownButton>
+         </div>
+        <div className="col-md-1">
+          <DropdownButton title="Division" id="leagues-nested-dropdown">
+            <MenuItem eventKey="1">East</MenuItem>
+            <MenuItem eventKey="2">Central</MenuItem>
+            <MenuItem eventKey="2">West</MenuItem>
+          </DropdownButton>
+         </div>
+        <div className="col-md-1">
+          <DropdownButton title="View" id="view-nested-dropdown">
+            <MenuItem eventKey="1">Batting Average</MenuItem>
+            <MenuItem eventKey="2">ERA</MenuItem>
+            <MenuItem eventKey="2"></MenuItem>
+          </DropdownButton>
+         </div>
+         <div className="col-md-1">
+          <Button href="/foo/bar" className="btn btn-default">Foo</Button>
+         </div>
       </div>
     );
   }
