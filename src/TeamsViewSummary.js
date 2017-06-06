f5370a419b61c9e8834bffd8e2d19d55d72fb0cf
Jeff Dufresne <jeffery.dufresne@gmail.com>
2017-06-06 16:02:59 -0400

    able to get teams and save in state for charting

--- src/TeamsViewSummary.js
+++ src/TeamsViewSummary.js
@@ -6,7 +6,19 @@ class TeamsViewSummary extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      teams: []
     };
+  }
+  componentWillReceiveProps(nextProps) {
+    let self = this;
+    if(nextProps.year && nextProps.league) {
+      baseballDataServices.teams(nextProps.year, nextProps.league, nextProps.division)
+        .then(function(teams) {
+          self.setState({
+            teams: teams
+          });
+        });
+    }
   }
   render() {
     return (
@@ -15,6 +27,13 @@ class TeamsViewSummary extends Component {
         <div>{this.props.league}</div>
         <div>{this.props.division}</div>
         <div>{this.props.view}</div>
+        {this.state.teams.map( function(team){
+          return (
+            <div key={team.teamID}>
+              <span>{team.name}</span>
+            </div>
+          );
+        })}
       </Panel>
     );
   }
